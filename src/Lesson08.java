37f797d25bb3d495aa70991c8ddff4c113b96c54
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-05-21 18:58:50 -0300

    feat: Lesson 08 - number diferent of 10

---
+++ src/Lesson08.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Lesson08 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        while (num != 10) {
+            System.out.println("Digite um número:");
+            num = sc.nextInt();
+        }
+
+    }
+}
