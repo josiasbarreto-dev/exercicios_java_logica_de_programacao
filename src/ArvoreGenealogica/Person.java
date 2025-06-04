91d714ae8b0dc08f21706a12b387a4e1e01e58e9
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-06-04 15:07:22 -0300

    refactor: Adds Application class to organize the code and make it more readable.

--- src/ArvoreGenealogica/Person.java
+++ src/ArvoreGenealogica/Person.java
@@ -64,19 +64,4 @@ public class Person {
             System.out.println("Unknown mother");
         }
     }
-
-    public static void main(String[] args) {
-        Person maternalGrandmother = new Person("Maria Conceição", 57, null, null);
-        Person maternalGrandfather = new Person("João Carlos", 58, null, null);
-
-        Person paternalGrandmother = new Person("Ivanete", 55, null, null);
-        Person paternalGrandfather = new Person("Genilso", 50, null, null);
-
-        Person mother = new Person("Thamirys", 28, paternalGrandmother, paternalGrandfather);
-        Person father = new Person("Josias", 30, paternalGrandmother, paternalGrandfather);
-
-        Person son = new Person("Davi Lucca", 1, mother, father);
-
-        son.printTree();
-    }
 }
