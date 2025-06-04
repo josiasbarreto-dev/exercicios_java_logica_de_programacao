71b93f00c77770a1669b46b55b7b0f5c95df632b
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-06-04 12:38:21 -0300

    refactor: Modifies the inputs to receive user data immediately after choosing the operation option, avoiding code repetition in the program.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,7 +2,40 @@ import java.util.Scanner;
 
 public class Calculadora {
     public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        int option = getOption(scan);
+
+        int number1 = obterNumero1(scan);
+        int number2 = obterNumero2(scan);
+
+        switch (option){
+            case 1:
+                System.out.println("Soma");
+                System.out.println(somar(number1, number2));
+                break;
+            case 2:
+                System.out.println("Subtração");
+                System.out.println(subtrair(number1, number2));
+                break;
+            case 3:
+                System.out.println("Multiplicação");
+                System.out.println(multiplicar(number1, number2));
+                break;
+            case 4:
+                System.out.println("Divisão");
+                System.out.println(dividir(number1, number2));
+                break;
+            case 5:
+                System.out.println("Potenciação");
+                System.out.println(calcularPotencia(number1, number2));
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+
+    }
+
+    public static int getOption(Scanner scan){
         System.out.println("-----------------------------------------------");
         System.out.println("------------- Calculadora Padrão --------------");
         System.out.println("-----------------------------------------------");
@@ -11,72 +44,42 @@ public class Calculadora {
         System.out.println("2) Subtração (-)");
         System.out.println("3) Multiplicação (*)");
         System.out.println("4) Divisão (/)");
-        int option = sc.nextInt();
+        System.out.println("5) Potenciação (^)");
 
-        switch (option){
-            case 1:
-                System.out.println("Soma");
+        return scan.nextInt();
+    }
 
-                System.out.println("Escolha o número 1:");
-                int n1 = sc.nextInt();
+    public static int obterNumero1(Scanner scan){
+        System.out.println("Escolha o número 1:");
+        return scan.nextInt();
+    }
 
-                System.out.println("Escolha o número 2:");
-                int n2 = sc.nextInt();
+    public static int obterNumero2(Scanner scan){
+        System.out.println("Escolha o número 2:");
+        return scan.nextInt();
+    }
 
-                System.out.println(somar(n1, n2));
-                break;
-            case 2:
-                System.out.println("Subtração");
+    public static int somar(int number1, int number2){
+        return number1 + number2;
+    }
 
-                System.out.println("Escolha o número 1:");
-                n1 = sc.nextInt();
+    public static int subtrair(int number1, int number2){
+        return number1 - number2;
+    }
 
-                System.out.println("Escolha o número 2:");
-                n2 = sc.nextInt();
+    public static int multiplicar(int number1, int number2){
+        return number1 * number2;
+    }
 
-                System.out.println(subtrair(n1, n2));
-                break;
-            case 3:
-                System.out.println("Multiplicação");
-
-                System.out.println("Escolha o número 1:");
-                n1 = sc.nextInt();
-
-                System.out.println("Escolha o número 2:");
-                n2 = sc.nextInt();
-
-                System.out.println(multiplicar(n1, n2));
-                break;
-            case 4:
-                System.out.println("Divisão");
-
-                System.out.println("Escolha o número 1:");
-                n1 = sc.nextInt();
-
-                System.out.println("Escolha o número 2:");
-                n2 = sc.nextInt();
-
-                System.out.println(dividir(n1, n2));
-                break;
-            default:
-                System.out.println("Opção inválida!");
+    public static int dividir(int number1, int number2){
+        if (number2 == 0) {
+            System.out.println("Divisão por zero não é permitida.");
+            throw new ArithmeticException("Divisão por zero não é permitida.");
         }
-
+        return number1 / number2;
     }
 
-    public static int somar(int n1, int n2){
-        return n1 + n2;
-    }
-
-    public static int subtrair(int n1, int n2){
-        return n1 - n2;
-    }
-
-    public static int multiplicar(int n1, int n2){
-        return n1 * n2;
-    }
-
-    public static int dividir(int n1, int n2){
-        return n1 / n2;
+    public static int calcularPotencia(int base, int expoente) {
+              return (int) Math.pow(base, expoente);
     }
 }
